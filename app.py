2fab127bd16b0efce2c56e8084f2eac8a58706e5
subho <subho20042021@gmail.com>
2025-03-19 13:18:19 +0530

    initialization problem solved

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ import firebase_admin
 from firebase_admin import firestore, credentials
 
 
+cred = credentials.Certificate('admin.json')
+firebase_admin.initialize_app(cred)
+
 app = Flask(__name__)
 
 
@@ -12,8 +15,7 @@ def hello_world():  # put application's code here
 
 @app.route('/data', methods = ['POST'])
 def getDepartmentData():
-    cred = credentials.Certificate('admin.json')
-    firebase_admin.initialize_app(cred)
+
 
     db = firestore.client()
 
