734fdfd6581f0b6b653af183dd201f453ba7a3ae
subho <subho20042021@gmail.com>
2025-03-19 13:09:41 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, jsonify
+import firebase_admin
+from firebase_admin import firestore, credentials
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return 'Hello World!'
+
+@app.route('/data', methods = ['POST'])
+def getDepartmentData():
+    cred = credentials.Certificate('admin.json')
+    firebase_admin.initialize_app(cred)
+
+    db = firestore.client()
+
+    db_ref = db.collection('Department').stream()
+    return jsonify(list(map(lambda doc: doc.to_dict(), db_ref)))
+
+
+
+
+
+if __name__ == '__main__':
+    app.run()
